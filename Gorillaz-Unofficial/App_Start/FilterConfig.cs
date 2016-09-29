b36173b83347ceab0fa70c5ef1096418fe82ecc0
Michael Letcher <Michael.Letcher@coliban.com.au>
2016-09-29 13:41:37 +1000

    First Commit

---
+++ Gorillaz-Unofficial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Gorillaz_Unofficial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Gorillaz-Unofficial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gorillaz_Unofficial.MvcApplication" Language="C#" %>
