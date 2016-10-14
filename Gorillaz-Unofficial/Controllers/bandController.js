b778d2a1473ef39e04fb8e80558cb244c7e99b49
Michael Letcher <mtrletcher@hotmail.com>
2016-10-15 10:32:59 +1100

    updated band, removed robot noodle, added new images (more to come). Updated UI for new theme

--- Gorillaz-Unofficial/Controllers/bandController.js
+++ Gorillaz-Unofficial/Controllers/bandController.js
@@ -1,24 +1,33 @@
 ﻿app.controller('bandController', function ($scope) {
+    $scope.phase = 1;
     $scope.members = [
             {
                 name: 'Murdoc',
-                imageURL: 'Images/Murdoc.jpg'
+                imagePhase1: 'Images/Murdoc.jpg',
+                imagePhase2: 'Images/Murdoc.jpg',
+                imagePhase3: 'Images/Murdoc.jpg',
+                imagePhase4: 'Images/Murdoc.jpg'
             },
             {
                 name: '2-D',
-                imageURL: 'Images/2D.jpg'
+                imagePhase1: 'Images/2D.jpg',
+                imagePhase2: 'Images/2D.jpg',
+                imagePhase3: 'Images/2D.jpg',
+                imagePhase4: 'Images/2D.jpg'
             },
             {
                 name: 'Noodle',
-                imageURL: 'Images/Noodle.jpg'
+                imagePhase1: 'Images/Noodle.jpg',
+                imagePhase2: 'Images/Noodle.jpg',
+                imagePhase3: 'Images/Noodle.jpg',
+                imagePhase4: 'Images/Noodle.jpg'
             },
             {
                 name: 'Russel',
-                imageURL: 'Images/Russel.jpg'
-            },
-            {
-                name: 'Cyborg Noodle',
-                imageURL: 'Images/Cyborg_Noodle2.jpg'
+                imagePhase1: 'Images/Russel.jpg',
+                imagePhase2: 'Images/Russel.jpg',
+                imagePhase3: 'Images/Russel.jpg',
+                imagePhase4: 'Images/Russel.jpg'
             }
     ];
 });
