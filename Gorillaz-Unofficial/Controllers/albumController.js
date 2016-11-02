e78ae685203b7554e952ea9958f06f4e079e4e75
Michael Letcher <Michael.Letcher@coliban.com.au>
2016-11-02 17:02:15 +1100

    working on adapting OO nav system by Lyle

--- Gorillaz-Unofficial/Controllers/albumController.js
+++ Gorillaz-Unofficial/Controllers/albumController.js
@@ -1,26 +1,174 @@
-﻿app.controller("albumController", function ($scope, $firebaseObject) {
+﻿/* Created with assistence from Lyle Singleton */
+
+app.controller("albumController", function ($scope, $firebaseObject, $log) {
+    console.log('$scope',$scope);
+
     var fireRef = new Firebase("https://gorillaz-unofficial.firebaseio.com/");
-    
     var obj = $firebaseObject(fireRef);
-
-    $scope.view = 'albums';
-    $scope.breadcrumbs = [
-        {
-            name: 'Studio Albums',
-            view: 'albums',
-            active: true
-        }
-    ];
-
     // to take an action after the data loads, use the $loaded() promise
     obj.$loaded().then(function () {
-        console.log("loaded record:", obj);
+        console.log("$firebaseObject", obj);
 
         $scope.studioAlbums = obj.albums;
-        console.log("studioAlbums:", obj.albums);
+        
+        // Create View obj
+        $scope.view = new view();
+
+        //temp
+        $scope.view.AddNavigable(new Navigable("Studio Albums", "albums"), null);
+
+        // Initialise View
+        $scope.view.Init(obj);
 
         $("#loading").hide();
     });
+
+
+
+    /* Public UUID()
+     * Parameters: NA
+     * Return: (hopefully) unique string in the form of 'id-xxxxxxxxxxxxxxxxxxx'
+     */
+    function UUID() {
+        return 'id-' + Math.random().toString(36).substr(2, 16);
+    }
+
+    function Navigable(title, display) {
+        this.id = UUID(); // can be replaced with DB ids
+        this.title = title;
+        this.parent = null; // Navigable Object
+        this.children = []; // Navigable Objects
+        this.itemData = null; // Object
+        this.display = display;
+        /* Private AddChild()
+         * Info: Adds another Navigable to the Navigable.children array and wires up the child's Navigable.parent
+         * Parameters: newChildElement (Navigable ojbect)
+         * Return: NA
+         */
+        this.AddChild = function (newChildElement){
+            this.children.push(newChildElement);
+            newChildElement.parent = this;
+        };
+    }
+
+    function view() {
+        this.selectedNavigable = null;
+        this.rootNavigable = null;
+        this.navigables = [];
+        this.breadcrumbs = [];
+        this.currentdisplay = "";
+
+        // NavigableID can be omitted or a value held by a Navigable object 
+        // added to this object using View.AddNavigable()
+
+        /* Public init()
+         * Info: Initialises the View object and sets the currently selected Navigable
+         *       UPDATE, will read URL to select tree location & build tree from imported obj
+         * Parameters: selectedNavigableID (Navigable ojbect ID)
+         * Return: NA
+         */
+        this.Init = function (data) {
+            if (typeof data == 'undefined') {
+                // if data is not passed in, through error
+                $log.error('Error: Breadcrumb data undefined.');
+            } else {
+                //read data (only albums currently)
+                var lastID;
+                var lastNavObject = null;
+                angular.forEach(data.albums, function (value, key) {
+                    console.log(key, value);
+                    // Create nav object
+                    var lastNavObject = new Navigable("Studio Albums", "album");
+                    // Set ID of new object
+                    lastID = lastNavObject.id;
+                    // Set to view
+                    $scope.view.AddNavigable(new Navigable("Studio Albums", "album"), null);
+                });
+                // update tree
+                this.UpdateSelected(this.rootNavigable.id);
+            }
+        }
+
+
+        //  added to this object using View.AddNavigable()
+
+        /* Public UpdateSelected()
+         * Info: Updates View.selectedNavigable with the object with the id NavigableID
+         * Parameters: NavigableID (Navigable ojbect ID)
+         * Return: NA
+         */
+        this.UpdateSelected = function (navigableID) {
+            var newSelection = this.navigables[navigableID];
+
+            if (typeof newSelection != 'undefined') {
+                this.selectedNavigable = newSelection;
+                this.UpdateBreadcrumbs();
+                //this.HTMLBreadcrumbs();
+            }
+        }
+        
+        /* Public AddNavigable()
+         * Info: Adds a Navigatable to the navigation tree, parentNavigatable is null it will set the new object as the tree root
+         * Parameters: newNavigable (navigable ojbect), parentNavigable (navigable ojbect)
+         * Return: navigable object
+         */
+        this.AddNavigable = function (newNavigable, parentNavigable) {
+
+            if (parentNavigable != null) {
+                parentNavigable.AddChild(newNavigable);
+                this.navigables[newNavigable.id] = newNavigable;
+            }
+            else {
+                if (this.rootNavigable == null) {
+                    this.rootNavigable = newNavigable;
+                    this.navigables[newNavigable.id] = newNavigable;
+                }
+                else {
+                    return null;
+                }
+            }
+
+            return newNavigable;
+        };
+        
+        /* Public UpdateBreadcrumbs()
+         * Info: Updates the View.breadcrumbs array
+         * Parameters: NA
+         * Return: NA
+         */
+        this.UpdateBreadcrumbs = function () {
+            this.breadcrumbs = this.BuildBreadcrumbs();
+        }
+
+        /* Private BuildBreadcrumbs()
+         * Info: Generates an array of breadcrumbs with a title and id from the View.selectedNavigable
+         * Parameters: NA
+         * Return: result (array)
+         */
+        this.BuildBreadcrumbs = function () {
+            var result = [];
+            for (i = this.selectedNavigable; i != null; i = i.parent) {
+                result.unshift({
+                    title: i.title,
+                    id: i.id
+                });
+            }
+            return result;
+        }
+
+        //Not required
+        this.HTMLBreadcrumbs = function () {
+            var div = document.getElementById(this.breadcumbsPlaceholderID);
+            div.innerHTML = "<ol>";
+
+            for (i = 0; i < this.breadcrumbs.length; i++) {
+                div.innerHTML += '<li><a href="#" onclick="view.UpdateSelected(\'' + this.breadcrumbs[i].id + '\')">' + this.breadcrumbs[i].title + "</a></li>";
+            }
+
+            div.innerHTML += "</ol>";
+        }
+    }
+
 
     $scope.openAlbum = function (id) {
         var i = 0;
@@ -41,7 +189,6 @@
         });
         console.log("selectedAlbum: ", $scope.selectedAlbum);
     }
-
     $scope.openSong = function (id) {
         var i = 0;
         console.log(id);
@@ -100,6 +247,5 @@
         updateBreadcrumbs(viewName, view);
         $scope.view = view;
         console.log('Moving to ' + view);
-
     }
 });
