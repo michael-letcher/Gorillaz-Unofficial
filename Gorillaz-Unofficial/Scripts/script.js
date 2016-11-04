84e1d383f30510001fb207d7b3c803485659e2b0
Michael Letcher <Michael.Letcher@coliban.com.au>
2016-11-04 18:20:15 +1100

    minor changes to mobile view

--- Gorillaz-Unofficial/Scripts/script.js
+++ Gorillaz-Unofficial/Scripts/script.js
@@ -1,1 +1,7 @@
-﻿$.material.init();
+﻿// Initalise the material JS
+$.material.init();
+
+// Auto close nav
+$('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+});
